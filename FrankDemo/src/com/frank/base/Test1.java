ba01e5766333e40d803e7256ebc6551484529005
XuFrank <85281171@qq.com>
2017-10-17 15:51:42 +0800

    Format

--- FrankDemo/src/com/frank/base/Test1.java
+++ FrankDemo/src/com/frank/base/Test1.java
@@ -1,4 +1,5 @@
 package com.frank.base;
+
 public class Test1 {
 	public static void main(String[] args) {
 		Human aPerson = new Human(160);
@@ -9,11 +10,11 @@ public class Test1 {
 		aPerson.repeatBreath(10);
 		Human neZha = new Human(150, "shit");
 		System.out.println(neZha.getHeight());
-		
+
 		Women aWomen = new Women(180);
 		aWomen.growHeight(120);
 		System.out.println(aWomen.getHeight());
-		
+
 		System.out.println(Human.getPopulation());
 		Human bPerson = new Human(169);
 		System.out.println(bPerson.getPopulation());
